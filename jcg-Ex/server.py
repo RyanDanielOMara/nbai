e70a6c0c68dd0b4169dce7a13e6d3c7a2533ff69
Jeffreywye <jeffreywye7@gmail.com>
2017-09-28 02:37:10 -0400

    Added jinja templateing to index.html file and modified the server.py file from jcg-Ex to test the new dynamic table (Success of Test is still unknown)

--- jcg-Ex/server.py
+++ jcg-Ex/server.py
@@ -10,13 +10,19 @@ some_dict = {
     'Kobe Bryant' : ['LAL', 8,  81]
 }
 
+nbai = [
+['First Name', 'Last Name', 'Team', 'Position', 'ESPN Score', 'Our Predictions'],
+['LeBron', 'James', 'CLE', 'SF', '30', 'UnderValued'],
+['Isaiah', 'Thomas', 'CLE', 'PG', '25', 'OverValued']
+]
+
 @app.route('/', defaults={'path' : ''})
 @app.route('/<path:path>')
 def all_unnamed_routes(path):
     return flask.render_template(
-            'example.html',
-            player_dict=some_dict,
-            header_list=some_list
+            'index.html',
+            
+            WebsiteTable=nbai
     )
 
 
