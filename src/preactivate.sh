b553c074e2ad6902487a9708056e99b2e7388f1e
Matt <matt.alex.howard@gmail.com>
2017-11-14 12:11:03 -0500

    separated the files as as jon requested

--- src/preactivate.sh
+++ src/preactivate.sh
@@ -1,13 +1,8 @@
 if [[ $UID != 0 ]]; then
-    echo "Please start the script as root or sudo!"
-    exit 1
+	echo "Please start the script as root or sudo!"
+	exit 1
 fi
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-apt-get update
-apt-get install -y mongodb-org virtualenv python-pip
-virtualenv env
-env/bin/pip install flask
-env/bin/pip install mongokit
-env/bin/pip install nba_py
-env/bin/pip install -I pymongo==2.8
+chmod +x src/mongoinstall.sh
+chmod +x src/virtualenv_setup.sh
+./src/mongoinstall.sh
+./src/virtualenv_setup.sh
